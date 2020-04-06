23f469c72ad0a7d750ccf7c5dfd873d3876a4a41
Joe Stubbs <joestubbs5@gmail.com>
2020-04-06 11:29:32 -0500

    Several improvements here: 1) resolve tenant_id correctly based on base URL and session. 2) the WebAppTokenGen controll now calls the OAuth server (TokensResource) to generate tokens, not the Tokens API itself, and it calls it with the authorization code and client; 3) the OAuth server (TokensResource) now properly deletes the auth code when it is used; 4) The OAuth server allows password grant without client credentials (does not generate a refresh token though) 4) The OAuth server (TokensResource) now generates an access and refresh token on the authorization code and password grants, but only if OAuth client creds are provided in the case of the later.

--- service/controllers.py
+++ service/controllers.py
@@ -14,6 +14,7 @@ from service.ldap import list_tenant_users, get_tenant_user, check_username_pass
 
 # get the logger instance -
 from common.logs import get_logger
+
 logger = get_logger(__name__)
 
 
@@ -145,6 +146,7 @@ class SetTenantResource(Resource):
     each tenant simply uses its own base URL for the authorization server. This resource would be called before
     the Login resource is run.
     """
+
     def get(self):
         headers = {'Content-Type': 'text/html'}
 
@@ -173,6 +175,7 @@ class LoginResource(Resource):
     """
     Implements the URLs used by the Authorization server for logging a user into a specific tenant.
     """
+
     def get(self):
         client_id, client_redirect_uri, client_state, client = check_client()
         # selecting a tenant id is required before logging in -
@@ -180,7 +183,8 @@ class LoginResource(Resource):
         if not tenant_id:
             tenant_id = session.get('tenant_id')
         if not tenant_id:
-            logger.debug(f"did not find tenant_id in session; issuing redirect to SetTenantResource. session: {session}")
+            logger.debug(
+                f"did not find tenant_id in session; issuing redirect to SetTenantResource. session: {session}")
             return redirect(url_for('settenantresource',
                                     client_id=client_id,
                                     redirect_uri=client_redirect_uri,
@@ -202,8 +206,10 @@ class LoginResource(Resource):
             tenant_id = session.get('tenant_id')
         if not tenant_id:
             client_id, client_redirect_uri, client_state, client = check_client()
-            logger.debug(f"did not find tenant_id in session; issuing redirect to SetTenantResource. session: {session}")
-            raise errors.ResourceError("Invalid session; please return to the original application or logout of this session.")
+            logger.debug(
+                f"did not find tenant_id in session; issuing redirect to SetTenantResource. session: {session}")
+            raise errors.ResourceError(
+                "Invalid session; please return to the original application or logout of this session.")
         headers = {'Content-Type': 'text/html'}
         client_id = request.form.get('client_id')
         client_redirect_uri = request.form.get('client_redirect_uri')
@@ -244,6 +250,7 @@ class AuthorizeResource(Resource):
     name of the client requesting authorization and asks the user to approve it. This resource is only called once
     the user has authenticated.
     """
+
     def get(self):
         client_id, client_redirect_uri, client_state, client = check_client()
         if not 'username' in session:
@@ -265,7 +272,7 @@ class AuthorizeResource(Resource):
                    'client_redirect_uri': client_redirect_uri,
                    'client_state': client_state}
 
-        return make_response(render_template('authorize.html',  **context), 200, headers)
+        return make_response(render_template('authorize.html', **context), 200, headers)
 
     def post(self):
         # selecting a tenant id is required before logging in -
@@ -297,6 +304,12 @@ class AuthorizeResource(Resource):
                                        redirect_url=client.callback_url,
                                        code=AuthorizationCode.generate_code(),
                                        expiry_time=AuthorizationCode.compute_expiry())
+        try:
+            db.session.add(authz_code)
+            db.session.commit()
+        except Exception as e:
+            logger.error(f"Got exception trying to add and commit the auth code. e: {e}; type(e): {type(e)}")
+            raise errors.ResourceError("Internal error saving authorization code. Please try again later.")
         # issue redirect to client callback_url with authorization code:
         url = f'{client.callback_url}?code={authz_code}&state={state}'
 
@@ -311,39 +324,51 @@ class TokensResource(Resource):
     """
 
     def post(self):
+        logger.debug("top os POST /oauth2/tokens")
         validator = RequestValidator(utils.spec)
         result = validator.validate(FlaskOpenAPIRequest(request))
         if result.errors:
             raise errors.ResourceError(msg=f'Invalid POST data: {result.errors}.')
         validated_body = result.body
-        logger.debug(f"BODY: {validated_body}")
+        logger.debug(f"POST body validated; body: {validated_body}")
         data = Token.get_derived_values(validated_body)
 
-        grant_type = data['grant_type']
+        grant_type = data.get('grant_type')
         if not grant_type:
             raise errors.ResourceError(msg=f'Missing the required grant_type parameter.')
-
+        logger.debug(f"processing grant_type: {grant_type}")
         tenant_id = g.request_tenant_id
+
         # get headers
-        try:
-            auth = request.authorization
-            client_id = auth.username
-            client_key = auth.password
-        except Exception as e:
-            raise errors.ResourceError(msg=f'Invalid headers. Basic authentication with client id and k'
-                                           f'ey required but missing.')
+        auth = request.authorization
+        # client id and client key are optional on the password grant type to allow new users to generate tokens
+        # right away before they create a client
+        if not auth:
+            client_id = None
+            client_key = None
+        else:
+            try:
+                client_id = auth.username
+                client_key = auth.password
+            except Exception as e:
+                raise errors.ResourceError(msg='Invalid headers. Basic authentication with client id and key '
+                                               'required but missing.')
+        if not client_id and not client_key and grant_type == 'password':
+            logger.debug("Allowing the password grant request even the auth header missing.")
         # check that client is in db
-        logger.debug("Checking that client exists.")
-        client = Client.query.filter_by(tenant_id=tenant_id, client_id=client_id, client_key=client_key).first()
-        if not client:
-            raise errors.ResourceError(msg=f'Invalid client credentials: {client_id}, {client_key}.')
-        # check grant type:
+        else:
+            logger.debug("Checking that client exists.")
+            client = Client.query.filter_by(tenant_id=tenant_id, client_id=client_id, client_key=client_key).first()
+            if not client:
+                raise errors.ResourceError(msg=f'Invalid client credentials: {client_id}, {client_key}.')
+
+        # checks by grant type:
         if grant_type == 'password':
             # validate user/pass against ldap
             username = data.get('username')
             password = data.get('password')
             if not username or not password:
-                raise errors.ResourceError("Missing requried payload data; username and password are required for "
+                raise errors.ResourceError("Missing required payload data; username and password are required for "
                                            "the password grant type.")
             check_ldap = check_username_password(tenant_id, username, password)
             logger.debug(f"returned: {check_ldap}")
@@ -359,25 +384,51 @@ class TokensResource(Resource):
             code = data.get('code')
             if not code:
                 raise errors.ResourceError("Required authorization_code parameter missing.")
-            AuthorizationCode.validate_code(tenant_id=tenant_id,
-                                            code=code,
-                                            client_id=client_id,
-                                            client_key=client_key)
+            # this server MUST expire the authorization code after a single use; multiple uses of the same
+            # authorization code are NOT permitted by the OAuth2 spec: https://tools.ietf.org/html/rfc6749#section-4.1.2
+            AuthorizationCode.validate_and_consume_code(tenant_id=tenant_id,
+                                                        code=code,
+                                                        client_id=client_id,
+                                                        client_key=client_key)
         else:
+            logger.debug(f"Invalid grant_type: {grant_type}")
             raise errors.ResourceError("Invalid grant_type")
 
         # call /v3/tokens to generate access token for the user
+        # TODO -- update to use the service tapy client.
         url = f'{g.request_tenant_base_url}/v3/tokens'
         content = {
             "token_tenant_id": f"{tenant_id}",
-            "account_type": "service",
+            "account_type": "user",
             "token_username": f"{data['username']}",
+            "claims": {
+                "client_id": client_id,
+                "grant_type": grant_type,
+            },
+            # access token expires in 4 hours
+            "access_token_ttl": 14400,
         }
-        r = requests.post(url, json=content)
-        json_resp = json.loads(r.text)
+        # only generate a refresh token when OAuth client is passed
+        if client_id and client_key:
+            content["generate_refresh_token"] = True
+            # refresh token expires in 1 year
+            content["refresh_token_ttl"] = 31536000
 
-        return utils.ok(result=json_resp, msg="Token created successfully.")
-
+        # set the redirect_uri claim when using a web-based flow
+        if grant_type == 'authorization_code':
+            content['claims']["redirect_uri"] = redirect_uri
+        try:
+            r = requests.post(url, json=content)
+        except Exception as e:
+            logger.error(f"Got exception trying to POST to /v3/tokens endpoint. Exception: {e}")
+            raise errors.ResourceError("Failure to generate an access token; please try again later.")
+        try:
+            json_resp = json.loads(r.text)
+        except Exception as e:
+            logger.error(f"Got exception trying to get JSON data from POST to /v3/tokens endpoint. Exception: {e}")
+            raise errors.ResourceError("Failure to generate an access token - invalid JSON; please try again later.")
+        # return the token response
+        return utils.ok(result=json_resp['result'], msg="Token created successfully.")
 
 
 # ------------------
@@ -432,8 +483,8 @@ class WebappTokenAndRedirect(Resource):
             return make_response(render_template('token-display.html', **context), 200, headers)
         # otherwise, if there is no token in the session, start the OAuth2 flow with a redirect ---
         # redirect to login (oauth2/authorize)
-            # maybe pass csrf token as well (state var)
-            # get tenant_id based on url
+        # maybe pass csrf token as well (state var)
+        # get tenant_id based on url
         # http://localhost:5000/v3/oauth2/authorize?client_id=test_client&redirect_uri=http://localhost:5000/oauth2/webapp/callback&response_type=code
         # todo - in general, do not want to hard-code "dev.develop..."
         tokenapp_client = get_tokenapp_client()
@@ -460,6 +511,7 @@ class WebappTokenGen(Resource):
     def get(self):
         client_data = get_tokenapp_client()
         client_id = client_data['client_id']
+        client_key = client_data['client_key']
         client_redirect_uri = client_data['callback_url']
         # the user should already be authenticated and in the session --
         username = session.get('username')
@@ -468,25 +520,45 @@ class WebappTokenGen(Resource):
                          "find username in the session! ")
             raise errors.ResourceError(msg=f'The username could not be established from the session.')
         tenant_id = g.request_tenant_id
-        logger.debug(f"Client ID: {client_id} - tenant_id: {tenant_id}")
+        logger.debug(f"client_id: {client_id}; tenant_id: {tenant_id}")
+        # get additional query parameters from request ---
+        state = request.args.get('state')
+        code = request.args.get('code')
 
         #  POST to oauth2/tokens (passing code, client id, client secret, and redirect uri)
-            # redirect uri is just callback url
-        base_url = g.request_tenant_base_url
-        logger.debug(f"HERE IS THE BASE URL: {base_url}")
-        url = f'{base_url}/v3/tokens'
-        content = {
-            "token_tenant_id": tenant_id,
-            "account_type": "user",
-            "token_username": username,
-            "claims": {
-                "client_id": client_id,
-                "redirect_uri": client_redirect_uri,
-            }
-        }
-        r = requests.post(url, json=content)
-        json_resp = json.loads(r.text)
+        # redirect uri is just callback url
 
+        # TODO -- this request should be going to /v3/oauth2/tokens, passing the client_id, client_secret, code
+        logger.debug(f"request.base_url: {request.base_url}")
+        base_url = g.request_tenant_base_url
+        # todo -- is this the correct base_url? it needs to target the authorization server on the same URL as the
+        #         Token Webapp itself; e.g., if Token Webapp listening on localhost, base_url should be localhost
+        #                             if request to Token Webapp is to tacc.develop, base_url should be tacc.develop
+
+        # if the authenticator is running locally, use "localhost" for baseurl to interact with OAuth server:
+        if 'localhost' in request.base_url:
+            logger.debug("using localhost for base_url.")
+            base_url = 'http://localhost:5000'
+        logger.debug(f"Final base_url: {base_url}")
+
+        url = f'{base_url}/v3/oauth2/tokens'
+        content = {
+            "grant_type": "authorization_code",
+            "redirect_uri": client_redirect_uri,
+            "code": code
+        }
+        try:
+            r = requests.post(url, json=content, auth=(client_id, client_key))
+        except Exception as e:
+            logger.error(f"Got exception trying to POST to /v3/oauth2/tokens endpoint. Exception: {e}")
+            raise errors.ResourceError("Failure to generate an access token; please try again later.")
+        try:
+            json_resp = json.loads(r.text)
+        except Exception as e:
+            logger.error(f"Got exception trying to parse JSON from POST to /v3/tokens endpoint. Exception: {e}")
+            raise errors.ResourceError("Failure to generate an access token; please try again later.")
+
+        logger.debug(f"Made request successfully and got JSON. Now parsing JSON data: {json_resp}")
         # Get token from POST response
         token = json_resp['result']['access_token']['access_token']
         session['access_token'] = token
@@ -503,7 +575,8 @@ class LogoutResource(Resource):
         if not tenant_id:
             tenant_id = session.get('tenant_id')
         if not tenant_id:
-            logger.debug(f"did not find tenant_id in session; issuing redirect to SetTenantResource. session: {session}")
+            logger.debug(
+                f"did not find tenant_id in session; issuing redirect to SetTenantResource. session: {session}")
             # reset the session in case there is some weird cruft
             session.pop('username', None)
             session.pop('tenant_id', None)
@@ -525,5 +598,3 @@ class LogoutResource(Resource):
 class StaticFilesResource(Resource):
     def get(self, path):
         return send_from_directory('templates', path)
-
-
