b806bbf9fdd6b9c84c3a14f68c51349a12ef0169
Joe Stubbs <joestubbs5@gmail.com>
2020-04-11 17:54:51 -0500

    add tests to cover token validation in auth code and password grants.

--- service/tests/basic_test.py
+++ service/tests/basic_test.py
@@ -2,17 +2,22 @@ from base64 import b64encode
 import datetime
 import pytest
 import json
-from unittest import TestCase
+
+from common.auth import validate_token
 from service.api import app
 from service import models
-from common import auth
+
+
 
 # These tests are intended to be run locally.
 
 # client id and key for the test suite. a client with these credentials is added by the test suite at start up.
+TEST_TENANT_ID = 'dev'
 TEST_CLIENT_ID = 'tapis_authn_test_suite_client_id'
 TEST_CLIENT_KEY = 'Dkrio2odj2AbvR'
 TEST_CLIENT_REDIRECT_URI = 'http://localhost:5000/testsuite'
+TEST_USERNAME = 'testuser1'
+TEST_PASSWORD = 'testuser1'
 
 @pytest.fixture
 def client():
@@ -22,7 +27,8 @@ def client():
 @pytest.fixture(scope='module')
 def init_db():
     with app.app_context():
-        data = {'tenant_id': 'dev',
+        # add a test client to be used in all the tests
+        data = {'tenant_id': TEST_TENANT_ID,
                 "username": "tapis-authn-testsuite",
                 'client_id': TEST_CLIENT_ID,
                 'client_key': TEST_CLIENT_KEY,
@@ -37,8 +43,18 @@ def init_db():
             client_id=data['client_id'],
             client_key=data['client_key']
         ).first()
+        # if it is somehow not there, we are in real trouble; just bail out.
         if not client:
             assert False
+
+def teardown_module():
+    # clean up all the mess we made
+    with app.app_context():
+        models.AuthorizationCode.query.filter_by(tenant_id=TEST_TENANT_ID,
+                                                 client_id=TEST_CLIENT_ID,
+                                                 client_key=TEST_CLIENT_KEY).delete()
+        models.db.session.commit()
+
 
 def get_basic_auth_header(username, password):
     """
@@ -47,6 +63,43 @@ def get_basic_auth_header(username, password):
     user_pass = bytes(f"{username}:{password}", 'utf-8')
     return 'Basic {}'.format(b64encode(user_pass).decode())
 
+
+def validate_access_token(response):
+    """
+    Validate the a response has an access token and it is properly formatted.
+    """
+    assert 'access_token' in response.json['result']['access_token']
+    assert 'expires_at' in response.json['result']['access_token']
+    assert 'expires_in' in response.json['result']['access_token']
+    assert 'jti' in response.json['result']['access_token']
+    claims = validate_token(response.json['result']['access_token']['access_token'])
+    assert claims['tapis/tenant_id'] == TEST_TENANT_ID
+    assert claims['tapis/username'] == TEST_USERNAME
+    assert claims['sub'] == f'{TEST_USERNAME}@{TEST_TENANT_ID}'
+    return claims
+
+
+def validate_refresh_token(response):
+    """
+    Validate that a response has a refresh token and it is properly formatted.
+    """
+    assert 'refresh_token' in response.json['result']['refresh_token']
+    assert 'expires_at' in response.json['result']['refresh_token']
+    assert 'expires_in' in response.json['result']['refresh_token']
+    assert 'jti' in response.json['result']['refresh_token']
+    claims = validate_token(response.json['result']['refresh_token']['refresh_token'])
+    assert claims['tapis/token_type'] == 'refresh'
+    assert claims['tapis/tenant_id'] == TEST_TENANT_ID
+    # the refresh token embeds the access token claims within:
+    assert 'tapis/access_token' in claims
+    print(claims['tapis/access_token'])
+    assert claims['tapis/access_token']['sub'] == f'{TEST_USERNAME}@{TEST_TENANT_ID}'
+    return claims
+
+
+# =====================
+# Actual test functions
+# =====================
 
 def test_invalid_post(client):
     with client:
@@ -123,7 +176,7 @@ def test_password_grant_invalid_user_pass(client, init_db):
         auth_header = {'Authorization': get_basic_auth_header(TEST_CLIENT_ID, TEST_CLIENT_KEY)}
         payload = {
             'grant_type': 'password',
-            'username': 'testuser1',
+            'username': TEST_USERNAME,
             'password': 'the_wrong_password'
         }
         response = client.post(
@@ -140,8 +193,8 @@ def test_password_grant_valid(client, init_db):
         auth_header = {'Authorization': get_basic_auth_header(TEST_CLIENT_ID, TEST_CLIENT_KEY)}
         payload = {
             'grant_type': 'password',
-            'username': 'testuser1',
-            'password': 'testuser1'
+            'username': TEST_USERNAME,
+            'password': TEST_PASSWORD
         }
         response = client.post(
             "http://localhost:5000/v3/oauth2/tokens",
@@ -151,24 +204,22 @@ def test_password_grant_valid(client, init_db):
         )
         assert response.status_code == 200
         assert 'access_token' in response.json['result']
-        # access_token attributes:
-        assert 'access_token' in response.json['result']['access_token']
-        assert 'expires_at' in response.json['result']['access_token']
-        assert 'expires_in' in response.json['result']['access_token']
-        assert 'jti' in response.json['result']['access_token']
+        # validate access_token:
+        claims = validate_access_token(response)
+        assert claims['client_id'] == TEST_CLIENT_ID
+        assert claims['grant_type'] == 'password'
 
+        # validate refresh_token:
         assert 'refresh_token' in response.json['result']
-        # refresh_token attributes:
-        assert 'refresh_token' in response.json['result']['refresh_token']
-        assert 'expires_at' in response.json['result']['access_token']
-        assert 'expires_in' in response.json['result']['access_token']
-        assert 'jti' in response.json['result']['access_token']
+        claims = validate_refresh_token(response)
+        assert claims['tapis/access_token']['client_id'] == TEST_CLIENT_ID
+        assert claims['tapis/access_token']['grant_type'] == 'password'
 
 def test_password_grant_no_client(client, init_db):
     payload = {
         'grant_type': 'password',
-        'username': 'testuser1',
-        'password': 'testuser1'
+        'username': TEST_USERNAME,
+        'password': TEST_PASSWORD
     }
     response = client.post(
         "http://localhost:5000/v3/oauth2/tokens",
@@ -177,25 +228,73 @@ def test_password_grant_no_client(client, init_db):
     )
     assert response.status_code == 200
     assert 'access_token' in response.json['result']
-    # access_token attributes:
-    assert 'access_token' in response.json['result']['access_token']
-    assert 'expires_at' in response.json['result']['access_token']
-    assert 'expires_in' in response.json['result']['access_token']
-    assert 'jti' in response.json['result']['access_token']
+    # validate access_token:
+    claims = validate_access_token(response)
+    assert claims['client_id'] == None
+    assert claims['grant_type'] == 'password'
     # when not using an oauth client, refresh tokens are not returned:
     assert 'refresh_token' not in response.json['result']
 
 def test_authorization_code(client, init_db):
     # simulate the authorization approval -
-    response = client.post('http://localhost:5000/v3/oauth2/authorize',
-                           data={'tenant_id': 'dev',
-                                 'approve': True,
-                                 'client_id': TEST_CLIENT_ID,
-                                 'client_redirect_uri': TEST_CLIENT_REDIRECT_URI,
-                                 })
-    assert response.status_code == 302
-    # print(response.data)
-    # note: response.data is a raw bytes object containing the full HTML returned from the page.
-    response_str = response.data.decode('utf-8')
-    assert 'code=' in response_str
-    assert 'state=' in response_str
+    with client:
+        # use hte session_transaction to enable modification of the session object:
+        # cf., https://flask.palletsprojects.com/en/1.1.x/testing/#accessing-and-modifying-sessions
+        with client.session_transaction() as sess:
+            sess['username'] = TEST_USERNAME
+        # once we leave the context, session updates applied via sess object are available -
+        response = client.post('http://localhost:5000/v3/oauth2/authorize',
+                               data={'tenant_id': TEST_TENANT_ID,
+                                     'approve': True,
+                                     'client_id': TEST_CLIENT_ID,
+                                     'client_redirect_uri': TEST_CLIENT_REDIRECT_URI,
+                                     })
+        assert response.status_code == 302
+        # note: response.data is a raw bytes object containing the full HTML returned from the page.
+        # try this if you want to debug ===>  print(response.data)
+        response_str = response.data.decode('utf-8')
+        assert 'code=' in response_str
+        assert 'state=' in response_str
+        # look up the authorization_code in the db:
+        auth_code = models.AuthorizationCode.query.filter_by(tenant_id=TEST_TENANT_ID,
+                                                             client_id=TEST_CLIENT_ID,
+                                                             client_key=TEST_CLIENT_KEY,
+                                                             username=TEST_USERNAME).first()
+        assert auth_code.tenant_id == TEST_TENANT_ID
+        assert auth_code.username == TEST_USERNAME
+        assert auth_code.client_id == TEST_CLIENT_ID
+        assert auth_code.client_key == TEST_CLIENT_KEY
+        assert f'code={auth_code.code}' in response_str
+
+def test_authorization_code_grant(client, init_db):
+    with client:
+        # look up the authorization_code from the previous test:
+        auth_code = models.AuthorizationCode.query.filter_by(tenant_id=TEST_TENANT_ID,
+                                                             client_id=TEST_CLIENT_ID,
+                                                             client_key=TEST_CLIENT_KEY,
+                                                             username=TEST_USERNAME).first()
+        headers = {'Authorization': get_basic_auth_header(TEST_CLIENT_ID, TEST_CLIENT_KEY)}
+        data = {
+            'grant_type': 'authorization_code',
+            'code': auth_code.code,
+            'redirect_uri': TEST_CLIENT_REDIRECT_URI
+        }
+        rs = client.post("http://localhost:5000/v3/oauth2/tokens",
+                         headers=headers,
+                         data=json.dumps(data),
+                         content_type='application/json')
+        assert rs.status_code == 200
+        assert 'access_token' in rs.json['result']
+        # validate access_token:
+        claims = validate_access_token(rs)
+        assert claims['client_id'] == TEST_CLIENT_ID
+        assert claims['grant_type'] == 'authorization_code'
+        assert claims['redirect_uri'] == TEST_CLIENT_REDIRECT_URI
+        # refresh tokens are returned on authorization_code grant:
+        assert 'refresh_token' in rs.json['result']
+        # refresh_token attributes:
+        claims = validate_refresh_token(rs)
+        assert claims['tapis/access_token']['client_id'] == TEST_CLIENT_ID
+        assert claims['tapis/access_token']['grant_type'] == 'authorization_code'
+        assert claims['tapis/access_token']['redirect_uri'] == TEST_CLIENT_REDIRECT_URI
+
