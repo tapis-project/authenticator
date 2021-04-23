3fee9d76b74df9f6cb31c67f3e5a7d0facd21467
Steve Black <scblack@tacc.utexas.edu>
2021-04-23 17:11:58 -0500

    Update postgres url for sqlalchemy update.

--- service/models.py
+++ service/models.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 # app.secret_key = b"\x00" + secrets.token_bytes(12) + b"\x00"
 app.secret_key = b"AGHsjfh!#%$SNFJqw"
 try:
-    full_db_url = f'postgres://{conf.postgres_user}:{conf.postgres_password}@{conf.sql_db_url}'
+    full_db_url = f'postgresql://{conf.postgres_user}:{conf.postgres_password}@{conf.sql_db_url}'
 except Exception as e:
     logger.error(f"Got exception trying to build full_db_ulr; e: {e}")
     raise e
