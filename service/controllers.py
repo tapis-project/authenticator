8328fa174b3a03da82319cb4bf4509d1f6349103
Gilbert Curbelo <gilbertcurbelo3@gmail.com>
2022-10-10 10:10:25 -0500

    Attempting to add CSRF

--- service/controllers.py
+++ service/controllers.py
@@ -5,7 +5,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 from flask import Flask, g, request, Response, render_template, redirect, make_response, send_from_directory, session, url_for
-from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CsrfProtect
 from flask_restful import Resource
 from openapi_core.shortcuts import RequestValidator
 from openapi_core.wrappers.flask import FlaskOpenAPIRequest
@@ -33,8 +33,9 @@ logger = get_logger(__name__)
 app = Flask(__name__)
 
 app.secret_key = b"AFHsjfh!#%$SNFJqw"
-csrf = CSRFProtect(app)
+csrf = CsrfProtect()
 
+csrf.init_app(app)
 # ------------------------------
 # REST API Endpoint controllers
 # ------------------------------
