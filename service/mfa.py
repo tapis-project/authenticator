c334022da6bda87d7c59603c7941577e7db8a0f7
Gilbert Curbelo <gilbertcurbelo3@gmail.com>
2023-03-23 10:59:07 -0500

    Viewing conf to test turn_off_mfa

--- service/mfa.py
+++ service/mfa.py
@@ -10,6 +10,8 @@ from tapisservice.logs import get_logger
 logger = get_logger(__name__)
 
 def needs_mfa(tenant_id):
+    logger.debug(conf)
+    logger.debug(str(conf))
     if conf.turn_off_mfa:
         return False
     logger.debug("checking if tenant needs mfa")
