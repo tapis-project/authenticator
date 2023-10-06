773124ac3479da2211681f78f480e951b0005078
Gilbert Curbelo <gilbertcurbelo3@gmail.com>
2023-10-06 10:42:24 -0500

    Fix how we check for value in mfa_config

--- service/mfa.py
+++ service/mfa.py
@@ -13,15 +13,12 @@ logger = get_logger(__name__)
 def needs_mfa(tenant_id, mfa_timestamp=None):
     if conf.turn_off_mfa:
         return False
-    logger.debug("checking if tenant needs mfa")
     tenant_config = tenant_configs_cache.get_config(tenant_id)
-    logger.debug(tenant_config.mfa_config)
 
     try:
         mfa_config = json.loads(tenant_config.mfa_config)
         expired = check_mfa_expired(mfa_config, mfa_timestamp)
     except Exception as e:
-        logger.debug(f"Error parsing mfa config: {e}")
         return False
 
     # mfa_config is a JSON object; if the tenant is not configured for MFA, then 
@@ -36,10 +33,9 @@ def check_mfa_expired(mfa_config, mfa_timestamp=None):
     Based on the tenant's MFA config and an optional MFA timestamp corresponding to the 
     last time an MFA was completed, determine whether the MFA session should be expired.
     """
-    logger.debug("Checking MFA expired")
     if mfa_timestamp is not None:
         if "tacc" in mfa_config:
-            if mfa_config['tacc']['expire']:
+            if 'expire' in mfa_config['tacc']:
                 current_time = time.time()
                 if current_time - mfa_timestamp > int(mfa_config['tacc']['expiry_frequency']):
                     return True
@@ -47,16 +43,12 @@ def check_mfa_expired(mfa_config, mfa_timestamp=None):
 
 
 def call_mfa(token, tenant_id, username):
-    logger.debug(f"calling mfa for: {username}")
     tenant_config = tenant_configs_cache.get_config(tenant_id)
 
     try:
         mfa_config = json.loads(tenant_config.mfa_config)
     except Exception as e:
-        logger.debug(f"Error parsing mfa config: {e}")
         return e
-    
-    logger.debug(f"Tenant mfa config: {mfa_config}")
 
     if not mfa_config:
         return ''
@@ -65,8 +57,6 @@ def call_mfa(token, tenant_id, username):
         return privacy_idea_tacc(mfa_config, token, username)
 
 def privacy_idea_tacc(config, token, username):
-    logger.debug("In privacy_idea_tacc function")
-
     if not config:
         return False
     
@@ -85,28 +75,21 @@ def privacy_idea_tacc(config, token, username):
         return verify_mfa_token(privacy_idea_url, jwt, token, username, realm)
 
 def get_privacy_idea_jwt(url, username, password):
-    logger.debug("Generating privacy idea JWT")
     data = {
         "username": username,
         "password": password
     }
     url = f"{url}/auth"
-    logger.debug(url)
     try:
         response = requests.post(url, json=data)
-        logger.debug(f"Response: {response}")
         response.raise_for_status()
     except Exception as e:
-        logger.debug(f"error: {e}")
         return
     jwt = response.json()['result']['value']['token']
-    logger.debug(jwt)
     return jwt
 
 def verify_mfa_token(url, jwt, token, username, realm):
-    logger.debug(f"Verifying MFA token: {token} for: {username}")
     url = f"{url}/validate/check"
-    logger.debug(url)
     data = {
         "user": username,
         "realm": realm,
@@ -117,10 +100,8 @@ def verify_mfa_token(url, jwt, token, username, realm):
     }
     try:
         response = requests.post(url, data=data, headers=headers)
-        logger.debug(f"Response: {response}")
         response.raise_for_status()
     except Exception as e:
-        logger.debug(f"error: {e}")
         return False
     valid = response.json()['result']['value']
     return valid
