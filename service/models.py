4be3cb9fb5aaa0a9b7ad6ef3229cf8a21b675c8c
kevinpricethesecond <kevinpricethesecond@gmail.com>
2026-08-11 11:48:47 -0500

    initial structure

--- service/models.py
+++ service/models.py
@@ -247,6 +247,8 @@ class TenantConfigsCache(object):
             return 'ldap'
         if 'globus' in custom_idp_config.keys():
             return 'globus'
+        if 'vdj' in custom_idp_config.keys():
+            return 'vdj'
         return None
 
     def get_mfa_type(self, tenant_id):
